364ec8e911dca0dfda67b4ff2c98434ad30f261f
cans1z <canss1zzz@gmail.com>
2023-12-12 22:19:12 +0300

    курва эжик

--- Adam/Assets/Scripts/Adam.cs
+++ Adam/Assets/Scripts/Adam.cs
@@ -25,19 +25,38 @@ public class Adam : MonoBehaviour
         _direction.x = Input.GetAxisRaw("Horizontal");
         _direction.y = Input.GetAxisRaw("Vertical");
         _rb.MovePosition(_rb.position + _direction * speed * Time.fixedDeltaTime);
+
+        if (transform.position.x <= -10f)
+        {
+            transform.position = new Vector2(9.99f, transform.position.y);
+        }
+
+        if (transform.position.x >= 10f)
+        {
+            transform.position = new Vector2(-9.99f, transform.position.y);
+        }
+
+        if (transform.position.y <= -5f)
+        {
+            transform.position = new Vector2(transform.position.x, 4.99f);
+        }
+
+        if (transform.position.y >= 5f)
+        {
+            transform.position = new Vector2(transform.position.x, -4.99f);
+        }
     }
 
     void FixedUpdate()
     {
-
-        Move(Speed);
+        Move(Speed);       
     }
+
     private void Update()
     {
-        Vector3 LocalScale = new Vector3();
-        LocalScale.x = LocalScale.x + 0.1f;
-        transform.localScale = LocalScale;
+       
     }
+
 
     //public void flip(bool fliped, float parametr)
     //{
