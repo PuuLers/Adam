1e2d810b386e1c29577e7cac2032f50b93ad0757
PuuLers <115350562+PuuLers@users.noreply.github.com>
2024-01-04 23:20:38 +0500

    menu cool

--- Adam/Assets/Scripts/Mechanisms/Menu.cs
+++ Adam/Assets/Scripts/Mechanisms/Menu.cs
@@ -1,25 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+    public GameObject preferencesPanel;
     public GameObject menuPanel;
-    public void ActivateMenu()
+    private bool _activePrefPanel = false;
+  
+
+
+
+
+
+    public void ActivatePreferencesPanel()
+    {
+        if (_activePrefPanel)
+        {
+            preferencesPanel.SetActive(true);
+        }
+        else
+        {
+            preferencesPanel.SetActive(false);
+        }
+        _activePrefPanel = !_activePrefPanel;
+    }
+
+
+    public void ActivateMenuButton()
     {
         if (Input.GetButtonDown("Cancel"))
         {
-            menuPanel.SetActive(true);
-            //Time.timeScale = 0f;
+            ActivateMenu();
         }
+    }
+
+    public void ActivateMenu()
+    {
+        menuPanel.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void Reastart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
     public void DeactivateMenu()
     {
         menuPanel.SetActive(false);
-        //Time.timeScale = 1f;
+        Time.timeScale = 1f;
+    }
+    private void Start()
+    {
+        ActivateMenu();
     }
     void Update()
     {
-        ActivateMenu();
+        ActivateMenuButton();
     }
 }
